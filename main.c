2d673bc95cd8896a85f38eaada94be9e61fd21f4
207z <3099401778@qq.com>
2026-09-24 13:18:14 +0800

    new feature

--- main.c
+++ main.c
@@ -5,5 +5,13 @@ int main()
     int a, b;
     scanf("%d %d", &a, &b);
     printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    if (b != 0) {
+        printf("%d\n", a / b);
+    }
+    else {
+        printf("Division by zero is not allowed.\n");
+    }
     printf("Hello, world!\n");
 }
